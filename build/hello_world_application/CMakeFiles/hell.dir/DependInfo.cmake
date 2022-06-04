d16fe1391edd411ebce0e332417efc47f80a464f
denjasan <denjasan@gmail.com>
2022-06-04 20:58:11 +0300

    LAB 03

---
+++ build/hello_world_application/CMakeFiles/hell.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/denjasan/TiMP/lab-03/lab03/hello_world_application/hello_world.cpp" "hello_world_application/CMakeFiles/hell.dir/hello_world.cpp.o" "gcc" "hello_world_application/CMakeFiles/hell.dir/hello_world.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/denjasan/TiMP/lab-03/lab03/build/formatter_ex_lib/CMakeFiles/formatter_ex.dir/DependInfo.cmake"
+  "/Users/denjasan/TiMP/lab-03/lab03/build/formatter_lib/CMakeFiles/formatter.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/solver_application/CMakeFiles/solve.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solve.dir/equation.cpp.o"
+  "CMakeFiles/solve.dir/equation.cpp.o.d"
+  "solve"
+  "solve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
